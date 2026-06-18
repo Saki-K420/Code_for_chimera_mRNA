d9181b875495ad08c304c00d5747c920ed1e7585
Saki K <sakikawakami.0420@gmail.com>
2026-06-18 11:30:50 +0900

    Update GFF2BED_TSS.py

--- GFF2BED_TSS.py
+++ GFF2BED_TSS.py
@@ -7,26 +7,26 @@ infile = str(argvs) ## input GTF file
 outfile = str(argvs[2]) ## output of TSS bed file
 
 with open(infile, 'r') as fr, open(outfile, 'w') as fw:
-    for line in fr:  #１行づつ読む
-        line = line.strip() # 行を文字列としてlineに代入する
+    for line in fr:  # Read each line from the GFF file
+        line = line.strip() # Substitute lines as a character
         
-        if line.startswith('#'):  # GFFファイルで"#"から始まる注釈はスキップ
+        if line.startswith('#'):  # Skip comment lines beginning with "#"
             continue
             
         else:
-            match = re.search(r'transcript_id\s+"([^"]+)"', line)  # line内にtranscript_id "XXXXX"というパターンがあるか
-            if match:  # transcript_id "XXXXX"というパターンがあったら
-                transcript_id = match.group(1)  # transcript_id "XXXXX"のXXXXXXの部分をtranscript_idに代入する
+            match = re.search(r'transcript_id\s+"([^"]+)"', line)  # Search for the pattern transcript_id "XXXXX"
+            if match:  # # If the line contains a pattern of "XXXXX",
+                transcript_id = match.group(1)  # extract "XXXXX" and store it in `transcript_id`
                 
-                columns = line.split('\t')  # 行をタブで区切ってリスト化してcolumnsに代入
+                columns = line.split('\t')  # Split the line by tabs and store the resulting list in `columns`.
                 
-                if columns[2] == 'transcript':  # もしcolumnsの2番め(リストは0始まり)が"transcript"だったら
+                if columns[2] == 'transcript':  # If columns[2] begin with "transcript"
                 
-                    if columns[6] == '+':  # プラス鎖の場合は転写物の5'端（columns[3]）の値で計算
-                        # bed座標の形に整えて行に出力
+                    if columns[6] == '+':  # Calculate the coordinate using the transcript 5' end as (columns[3]) for plus strand
+                        # Output as bed file
                         fw.write(f'{columns[0]}\t{int(columns[3])-1}\t{int(columns[3])}\t{transcript_id}\t0\t{columns[6]}\n')
                         
-                    elif columns[6] == '-':  # マイナス鎖の場合は転写物の3'端(columns[4])の値で計算
-                        # bed座標の形に整えて行に出力
+                    elif columns[6] == '-':  # Calculate the coordinate using the transcript 5' end as (columns[6]) for minus strand
+                        # Output as bed file
                         fw.write(f'{columns[0]}\t{int(columns[4])-1}\t{int(columns[4])}\t{transcript_id}\t0\t{columns[6]}\n')
 # %%
